785f02218217c9bbd9510075c15c1090cf528cde
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2021-05-08 21:06:50 -0300

    repair words

--- 04_Underfitting&Overfitting/Exercises.py
+++ 04_Underfitting&Overfitting/Exercises.py
@@ -58,7 +58,7 @@ candidate_max_leaf_nodes = [5, 25, 50, 100, 250, 500]
 scores = {leaf_size: get_mae(leaf_size, train_X, val_X, train_y, val_y) for leaf_size in candidate_max_leaf_nodes}
 
 # Store the best value of max_leaf_nodes (it will be either 5, 25, 50, 100, 250 or 500)
-best_tree_size = min(scores, key=scores.get)
+best_tree_size = min(scores, key = scores.get)
 
 # Check your answer
 step_1.check()
