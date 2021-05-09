074554712471e8e850e30446210ed695e6ec01f5
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2021-05-09 16:36:56 -0300

    exercise06 add

---
+++ 06_ML_Competitions/Exercises.py
@@ -0,0 +1,80 @@
+# This notebook is an exercise in the Introduction to Machine Learning course. 
+# You can reference the tutorial at this link.
+
+# Introduction
+# In this exercise, you will create and submit predictions for a Kaggle competition.
+#  You can then improve your model (e.g. by adding features) to improve and see 
+# how you stack up to others taking this course.
+
+# The steps in this notebook are:
+
+# Build a Random Forest model with all of your data (X and y).
+# Read in the "test" data, which doesn't include values for the target.
+# Predict home values in the test data with your Random Forest model.
+# Submit those predictions to the competition and see your score.
+# Optionally, come back to see if you can improve your model by adding features or 
+# changing your model. Then you can resubmit to see how that stacks up on the 
+# competition leaderboard.
+
+# Recap
+# Here's the code you've written so far. Start by running it again.
+
+# Code you have previously used to load data (Congratulations for your progress ;))
+
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeRegressor
+
+# Set up code checking
+import os
+if not os.path.exists("../input/train.csv"):
+    os.symlink("../input/home-data-for-ml-course/train.csv", "../input/train.csv")  
+    os.symlink("../input/home-data-for-ml-course/test.csv", "../input/test.csv") 
+from learntools.core import binder
+binder.bind(globals())
+from learntools.machine_learning.ex7 import *
+
+# Path of the file to read. We changed the directory structure to simplify 
+# submitting to a competition
+iowa_file_path = '../input/train.csv'
+
+home_data = pd.read_csv(iowa_file_path)
+# Create target object and call it y
+y = home_data.SalePrice
+# Create X
+features = ['LotArea', 'YearBuilt', '1stFlrSF', '2ndFlrSF', 'FullBath', 
+            'BedroomAbvGr', 'TotRmsAbvGrd']
+X = home_data[features]
+
+# Split into validation and training data
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=1)
+
+# Specify Model
+iowa_model = DecisionTreeRegressor(random_state=1)
+# Fit Model
+iowa_model.fit(train_X, train_y)
+
+# Make validation predictions and calculate mean absolute error
+val_predictions = iowa_model.predict(val_X)
+val_mae = mean_absolute_error(val_predictions, val_y)
+print("Validation MAE when not specifying max_leaf_nodes: {:,.0f}".format(val_mae))
+
+# Using best value for max_leaf_nodes
+iowa_model = DecisionTreeRegressor(max_leaf_nodes=100, random_state=1)
+iowa_model.fit(train_X, train_y)
+val_predictions = iowa_model.predict(val_X)
+val_mae = mean_absolute_error(val_predictions, val_y)
+print("Validation MAE for best value of max_leaf_nodes: {:,.0f}".format(val_mae))
+
+# Define the model. Set random_state to 1
+rf_model = RandomForestRegressor(random_state=1)
+rf_model.fit(train_X, train_y)
+rf_val_predictions = rf_model.predict(val_X)
+rf_val_mae = mean_absolute_error(rf_val_predictions, val_y)
+
+print("Validation MAE for Random Forest Model: {:,.0f}".format(rf_val_mae))
+
+# Creating a Model For the Competition
+# Build a Random Forest model and train it on all of X and y.
