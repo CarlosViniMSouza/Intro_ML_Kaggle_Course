472a0ebca1ffc5ab6687373dcdcac91091958634
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2021-05-09 16:42:13 -0300

    Course finish

--- 06_ML_Competitions/Exercises.py
+++ 06_ML_Competitions/Exercises.py
@@ -78,3 +78,34 @@ print("Validation MAE for Random Forest Model: {:,.0f}".format(rf_val_mae))
 
 # Creating a Model For the Competition
 # Build a Random Forest model and train it on all of X and y.
+
+# To improve accuracy, create a new Random Forest model which you will train on all training data
+rf_model_on_full_data = RandomForestRegressor(random_state=1)
+
+# fit rf_model_on_full_data on all data from the training data
+rf_model_on_full_data.fit(train_X, train_y)
+rf_model_on_full_data
+
+# # path to file you will use for predictions
+test_data_path = '../input/test.csv'
+
+# read test data file using pandas
+test_data = pd.read_csv(test_data_path)
+
+# create test_X which comes from test_data but includes only the columns you used for prediction.
+# The list of columns is stored in a variable called features
+test_X = test_data[features]
+
+# make predictions which we will submit. 
+test_preds = rf_model_on_full_data.predict(test_X)
+
+# The lines below shows how to save predictions in format used for competition scoring
+# Just uncomment them.
+
+output = pd.DataFrame({'Id': test_data.Id,
+                       'SalePrice': test_preds})
+output.to_csv('submission.csv', index=False)
+
+# Check your answer
+step_1.check()
+step_1.solution()
